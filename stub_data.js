dbd1670fd519b02937646abdc9aaa7cf82bcf76e
nagarajanceg <nagharajang@gmail.com>
2020-03-26 18:15:31 -0700

    adding stub data

---
+++ stub_data.js
@@ -0,0 +1,124 @@
+'use strict'
+module.exports = [
+	{
+		'id':2,
+		'name':'d',
+		'custom':'a',
+		ele:{
+			ind:3
+		},
+		str:{
+			val:'mith'
+		},
+		next:{
+			inner: {
+				inner_most:5
+			}
+		},
+		hdd: [
+			{
+				ids : [
+					 {
+						obj : {
+							find : 10
+						}
+					}
+				]
+			},
+			{
+				ids : 1
+			},
+			{
+				ids : 7
+			},
+			{
+				ids: 4
+			}
+		]
+	},
+	{
+		'id':1,
+		'name':'c',
+		'custom':'z',
+		ele:{
+			ind:3
+		},
+		str:{
+			val:'vel'
+		},
+		next:{
+			inner: {
+				inner_most:1
+			}
+		},
+		hdd: [
+			 {
+				ids : 5
+			},
+			{
+				ids : 3
+			},
+			{
+				ids : 6
+			}
+		]
+		
+	},
+	{
+		'id':4,
+		'name':'c',
+		'custom':'a',
+		ele:{
+			ind:5
+		},
+		str:{
+			val:'aj'
+		},
+		next:{
+			inner: {
+				inner_most:4
+			}
+		},
+		hdd: [
+			{
+				ids : 3
+			},
+			{
+				ids : 4
+			}
+		]
+
+	},
+	{
+		'id':3,
+		'name':'f',
+		'custom':'e',
+		ele:{
+			ind:5
+		},
+		str:{
+			val:'raj'
+		},
+		next:{
+			inner: {
+				inner_most:2
+			}
+		}
+	},
+	{
+		'id':5,
+		'name':'d',
+		'custom':'a',
+		ele:{
+			ind:2
+		},
+		str:{
+			val:"shak"
+		},
+		next:{
+			inner: {
+				inner_most:3
+			}
+		}
+	}
+]
